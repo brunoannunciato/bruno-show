a7d0bdaaa4d2cda2d44555751513e7eeba84b9ca
Bruno Annunciato <brunobannunciato@gmail.com>
2018-05-29 21:26:49 -0300

    add background effect + eu parallax

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ music.play();
     tudo.addEventListener('click', function() {
     music.play();
 });
+
+addEventListener('mousemove', function() {
+
+    if (document.querySelector('body').classList == '' ) {
+        document.querySelector('body').classList.add('bg-02');
+    } else {
+        document.querySelector('body').classList.remove('bg-02');
+    }
+})
